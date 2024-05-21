eb905233ce90316ad137d32e48ed0a1c9823fa2a
nadi-nikoko <nico.nardi@hotmail.com>
2024-05-21 09:59:01 +0200

    remove VideoDAOImp and extra html pages

--- random_rainbow/src/main/java/com/randomrainbow/springboot/demosecurity/dao/VideoDAO.java
+++ random_rainbow/src/main/java/com/randomrainbow/springboot/demosecurity/dao/VideoDAO.java
@@ -2,29 +2,85 @@ package com.randomrainbow.springboot.demosecurity.dao;
 
 import com.randomrainbow.springboot.demosecurity.entity.User;
 import com.randomrainbow.springboot.demosecurity.entity.Video;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.NoResultException;
+import jakarta.persistence.TypedQuery;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /*
-* DAO - Data Access Object
-* the DAO will store the methods from the app to interact with the database
-* in this case the implementation of videoDAO will be only one
-* but for good practices its necessary to create first an interface
-* and then its class implementation
-* that will @Autowired VideoDAO
-* */
-public interface VideoDAO {
+Spring @Repository annotation is used to indicate that
+the class provides the mechanism
+for storage, retrieval, search, update and delete
+operation on objects.
+*/
+@Repository
+public class VideoDAO {
 
-    List<Video> findAll();
+    // @Autowired is used for automatic dependency injection
+    // in this specific case its necessary because EntityManager is not a bean
+    @Autowired
+    // the EntityManager is the primary interface for managing entities in JPA
+    private EntityManager entityManager;
 
-    List<Video> findVideosByUserId(int userId);
+    public List<Video> findAll() {
+        // TypedQuery is a query to execute a query against the database
+        TypedQuery<Video> theQuery = entityManager.createQuery("FROM Video", Video.class);
+        List<Video> videos = theQuery.getResultList();
+        return videos;
+    }
 
-    Video findById(int id);
+    public List<Video> findVideosByUserId(int userId) {
+        // TypedQuery is a query to execute a query against the database
+        TypedQuery<Video> theQuery = entityManager
+                .createQuery("SELECT v FROM Video v JOIN v.user u WHERE u.id = :idUserVideo", Video.class);
+        theQuery.setParameter("idUserVideo", userId); // Corrected parameter name to match the query
+        List<Video> videos = theQuery.getResultList();
+        return videos;
+    }
 
-    Video save(Video video);
+    public List<Video> findVideosByUser(User idUser) {
+        TypedQuery<Video> theQuery = entityManager
+                .createQuery("SELECT v FROM Video v JOIN v.idUser u WHERE u = :idUserVideo", Video.class);
+        theQuery.setParameter("idUserVideo", idUser);
 
-    void deleteById(int id);
+        List<Video> videos = new ArrayList<>();
 
-    List<Video> findVideosByUser(User idUser);
+        try {
+            videos = theQuery.getResultList();
+        } catch (NoResultException e) {
+            System.out.println("No videos found for the user.");
+        } catch (Exception e) {
+            System.out.println("Error on finding videos: " + e.getMessage());
+        }
 
+        return videos;
+    }
+
+    public Video findById(int id) {
+        /*
+         * The EntityManager interface in JPA provides methods to manage entities,
+         * including persisting, merging, removing, and querying entities.
+         * It acts as a bridge between your application and the database,
+         * managing the lifecycle of entity instances.
+         */
+        Video video = entityManager.find(Video.class, id);
+        return video;
+    }
+
+    @Transactional
+    public Video save(Video video) {
+        Video videoDB = entityManager.merge(video);
+        return videoDB;
+    }
+
+    @Transactional
+    public void deleteById(int id) {
+        Video video = entityManager.find(Video.class, id);
+        entityManager.remove(video);
+    }
 }
